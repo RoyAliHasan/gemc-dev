84ded90812f5e7fd449454af59c8ac7f6233e169
Roy Ali Hasan <royalihasan0987@gmail.com>
2022-10-27 16:34:03 +0500

    Home Page Update

--- components/Buttons.js
+++ components/Buttons.js
@@ -4,13 +4,13 @@ import landmark from "../assets/images/Logomark.svg";
 import discord from "../assets/images/discord.svg";
 function Buttons() {
   return (
-    <div className="flex  items-center justify-center mt-10">
-      <button className="flex items-center justify-center   bg-gradient-to-r from-indigo-500 via-blue-500 to-blue-900 p-3 md:p-5 md:ml-8 ml-3 rounded-2xl text-white text-sm sm:text-xl">
+    <div className="flex  items-center justify-between mt-10">
+      <button className="flex items-center justify-center   bg-gradient-to-r from-indigo-500 via-blue-500 to-blue-900 p-3 md:p-5 md:ml-8  rounded-2xl text-white text-sm sm:text-xl">
         <Image className="mr-2" src={landmark} alt="landmark" />
         MINTING SOON
       </button>
       <button
-        className="flex items-center justify-center   bg-gradient-to-r from-indigo-500 via-blue-500 to-blue-900 ml-3  p-3 md:p-5 md:ml-8  rounded-2xl text-white text-sm sm:text-xl
+        className="flex items-center justify-center   bg-gradient-to-r from-indigo-500 via-blue-500 to-blue-900 ml-3  p-3 md:p-5 md:ml-8  rounded-2xl text-white text-sm sm:text-xl mr-2
       "
       >
         <Image className="mr-2 text-white" src={discord} alt={"discord"} />
